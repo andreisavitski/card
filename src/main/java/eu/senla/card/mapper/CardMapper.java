1f5a305045445b91bf8c6211be7533b02b688702
andreisavitski <andsavi93@gmai.com>
2024-10-25 17:26:59 +0300

    changes

--- src/main/java/eu/senla/card/mapper/CardMapper.java
+++ src/main/java/eu/senla/card/mapper/CardMapper.java
@@ -1,7 +1,5 @@
 package eu.senla.card.mapper;
 
-import eu.senla.card.dto.CardRequestDto;
-import eu.senla.card.dto.CardResponseDto;
 import eu.senla.card.dto.ClientCardResponse;
 import eu.senla.card.entity.Card;
 import org.mapstruct.Mapper;
@@ -9,12 +7,6 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CardMapper {
-
-    @Mapping(source = "client.id", target = "clientId")
-    CardResponseDto toDto(Card card);
-
-    @Mapping(source = "clientId", target = "client.id")
-    Card toCard(CardRequestDto cardRequestDto);
 
     @Mapping(source = "client.id", target = "clientId")
     ClientCardResponse toClientCardResponse(Card card);
--- src/main/java/eu/senla/card/service/CardService.java
+++ src/main/java/eu/senla/card/service/CardService.java
@@ -1,21 +1,13 @@
 package eu.senla.card.service;
 
-import eu.senla.card.dto.CardRequestDto;
-import eu.senla.card.dto.CardResponseDto;
 import eu.senla.card.dto.ClientCardResponse;
 import eu.senla.card.dto.TransferRequestMessage;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.TimeoutException;
 
 public interface CardService {
 
-    CardResponseDto addCard(CardRequestDto cardRequestDto);
-
     List<ClientCardResponse> findCardByClientId(Long clientId);
 
-    CardResponseDto getCardById(Long id);
-
-    boolean makeTransfer(TransferRequestMessage transferRequestMessage) throws IOException, InterruptedException, TimeoutException;
+    boolean makeTransfer(TransferRequestMessage transferRequestMessage);
 }
